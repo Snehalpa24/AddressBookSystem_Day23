2b00feb87aebb5c94c69cd61cf7906f4d1b193a4
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-10-16 21:03:11 +0530

    Duplicate person name not allowed in particular  address booko

--- AddressBookSystem_UC7.cs
+++ AddressBookSystem_UC7.cs
@@ -1,36 +1,13 @@
 using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 
 namespace AdressBookSystem
 {
-    public class AdressBookBuilder : IContacts
+    class AdressBookBuilder:IContacts
     {
-        public static string connectionString = @"Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = AddressBook; Integrated Security = True";
-        SqlConnection sqlConnection = new SqlConnection(connectionString);
-
-        /// <summary>
-        /// Checks the connection.
-        /// </summary>
-        public void checkConnection()
-        {
-            try
-            {
-                this.sqlConnection.Open();
-                Console.WriteLine("connection established");
-                this.sqlConnection.Close();
-            }
-            catch
-            {
-                Console.WriteLine("not established");
-            }
-        }
-
         public List<Contact> contactList;
-
         /// <summary>
         /// Initializes a new instance of the list <see cref="AdressBookBuilder"/> class.
         /// </summary>
@@ -38,7 +15,6 @@ namespace AdressBookSystem
         {
             this.contactList = new List<Contact>();
         }
-
         /// <summary>
         /// Adds the contact but contact is not be duplicated
         /// </summary>
@@ -52,16 +28,101 @@ namespace AdressBookSystem
         /// <param name="email">The email of person</param>
         public void addContact(string firstName, string lastName, string address, string city, string state, string zip, string phoneNumber, string email)
         {
-            bool duplicate = equals(firstName);
+            bool duplicate=equals(firstName);
             if (!duplicate)
             {
                 Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
                 contactList.Add(contact);
             }
-            else
+            else 
             {
                 Console.WriteLine("Cannot add duplicate contacts when you give first name same");
             }
         }
-}
+        /// <summary>
+        /// Equalses the specified first name for duplicate name.
+        /// </summary>
+        /// <param name="firstName">The first name.</param>
+        /// <returns></returns>
+        private bool equals(string firstName)
+        {
+            if (this.contactList.Any(e => e.firstName == firstName))
+                return true;
+            else
+                return false;
+        }
+        /// <summary>
+        /// Edits the contact with the help of first name of person
+        /// </summary>
+        /// <param name="firstName">The first nameof person</param>
+        public void editContact(string firstName)
+        {
+            int flag = 1;
+            foreach (Contact contact in contactList)
+            {
+                if (firstName.Equals(contact.firstName))
+                {
+                    flag = 0;
+                    Console.WriteLine("Enter last name = ");
+                    contact.lastName = Console.ReadLine();
+                    Console.WriteLine("Enter address= ");
+                    contact.address = Console.ReadLine();
+                    Console.WriteLine("Enter city= ");
+                    contact.city = Console.ReadLine();
+                    Console.WriteLine("Enter state= ");
+                    contact.state = Console.ReadLine();
+                    Console.WriteLine("Enter zip= ");
+                    contact.zip = Console.ReadLine();
+                    Console.WriteLine("Enter phoneNumber= ");
+                    contact.phoneNumber = Console.ReadLine();
+                    Console.WriteLine("Enter email= ");
+                    contact.email = Console.ReadLine();
+                    break;
+                }
+            }
+            if (flag == 1)
+            {
+                Console.WriteLine("Contact not found");
+            }
+        }
+        /// <summary>
+        /// Deletes the contact of person with the help of first name
+        /// </summary>
+        /// <param name="firstName">The first name of person</param>
+        public void deleteContact(string firstName)
+        {
+            int flag = 1;
+            foreach (Contact contact in contactList)
+            {
+                if (firstName.Equals(contact.firstName))
+                {
+                    flag = 0;
+                    contactList.Remove(contact);
+                    Console.WriteLine("Sucessfully deleted");
+                    break;
+                }
+            }
+            if (flag == 1)
+            {
+                Console.WriteLine("Contact not found");
+            }
+        }
+        /// <summary>
+        /// Displays the contact of persons
+        /// </summary>
+        public void displayContact()
+        {
+            foreach (Contact contact in contactList)
+            {
+                Console.WriteLine("\nFirst name = " + contact.firstName);
+                Console.WriteLine("Last name = " + contact.lastName);
+                Console.WriteLine("Address = " + contact.address);
+                Console.WriteLine("city = " + contact.city);
+                Console.WriteLine("state = " + contact.state);
+                Console.WriteLine("zip = " + contact.zip);
+                Console.WriteLine("phoneNumber = " + contact.phoneNumber);
+                Console.WriteLine("email = " + contact.email);
+            }
+        }
+    }
 }
